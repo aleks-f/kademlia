aa3cf1554a38f3697164d66af558a3ae4440fa82
Alex Fabijanic <alex@pocoproject.org>
2021-08-26 15:36:48 +0200

    chore(bootstrap): adjust to new session architecture (Poco::ActiveMethod/Result)

--- examples/bootstrap.cpp
+++ examples/bootstrap.cpp
@@ -12,9 +12,7 @@
 
 namespace k = kademlia;
 
-int main
-        ( int argc
-        , char** argv )
+int main(int argc, char** argv )
 {
     // Check command line arguments count
     if ( argc != 2 )
@@ -27,12 +25,7 @@ int main
     std::uint16_t const port = std::atoi( argv[1] );
 
     // Create the session
-    k::first_session session{ k::endpoint{ "0.0.0.0", port }
-                            , k::endpoint{ "::", port } };
-
-    // Start the main loop thread
-    auto main_loop = std::async( std::launch::async
-                               , &k::first_session::run, &session );
+    k::first_session session{k::endpoint{"0.0.0.0", port}, k::endpoint{"::", port}};
 
     // Wait for exit request
     std::cout << "Press any key to exit" << std::endl;
@@ -42,7 +35,7 @@ int main
     session.abort();
 
     // Wait for the main loop thread termination
-    auto failure = main_loop.get();
+    auto failure = session.wait();
     if ( failure != k::RUN_ABORTED )
         std::cerr << failure.message() << std::endl;
 }
